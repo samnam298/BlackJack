a54e6b6d5f384ec8c5dd6e22c6d5a149f03a6a9d
samnam298 <samrawithailena@gmail.com>
2025-10-23 10:21:04 -0400

    Contain 52 cards,Shuffle the deck,Deal cards

--- src/main/java/com/pluralsight/Deck.java
+++ src/main/java/com/pluralsight/Deck.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
+import java.util.*;
+
 public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        for (String suit : suits) {
+            for (String value : values) {
+                cards.add(new Card(suit, value));
+            }
+        }
+
+        shuffle();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        if (cards.isEmpty()) {
+            System.out.println("Deck is empty! Creating and shuffling a new one...");
+            return null;
+        }
+        Card dealt = cards.remove(0);
+        dealt.flip(); // make sure it’s face up when dealt
+        return dealt;
+    }
 }
